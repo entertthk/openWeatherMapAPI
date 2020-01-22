c9baa48289e92fc2edf513c7f8e6c41c027c62c1
entertthk <markkc2@hotmail.com>
2020-01-22 12:57:37 +0200

    owapi

---
+++ OpenWeatherMapAPI/WeatherDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMapAPI
+{
+    class WeatherDetails
+    {
+        public class Weather
+        {
+            public string Main { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class Sys
+        {
+            public string Country { get; set; }
+        }
+
+        public class Main
+        {
+            public double Temp { get; set; }
+        }
+        public class root
+        {
+            public Main Main { get; set; }
+            public Sys Sys { get; set; }
+            public List<Weather> Weather { get; set; }
+            public string Name { get; set; }
+        }
+
+    }
+}
